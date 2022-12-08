b59cb658423717c3efdaad530950448d37198c93
manuelamp <manuella.m.p182@gmail.com>
2022-12-08 14:55:48 -0300

    feat: all clients validations

---
+++ src/middlewares/clients.middlewares.js
@@ -0,0 +1,42 @@
+import connection from "../database/db.js";
+import { clientsSchema } from "../schemas/clients.schema.js";
+
+export async function clientValidation(req, res, next) {
+
+    const { cpf } = req.body;
+
+    const validationStatus = clientsSchema.validate(req.body, { abortEarly: false });
+
+    if (validationStatus.error) {
+        const error = validationStatus.error.details.map((detail) => detail.message);
+        res.status(400).send(error);
+        return;
+    };
+
+    try {
+        const isCpfExists = await connection.query(`SELECT * FROM customers WHERE cpf=$1`, [cpf]);
+
+        if (isCpfExists.rows.length !== 0) {
+            return res.sendStatus(409);
+        }
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+
+    next();
+}
+
+export async function isClientExists(req, res, next) {
+
+    const { id } = req.params;
+
+    try {
+        const isIdExists = await connection.query(`SELECT * FROM customers WHERE id=$1`, [id]);
+
+        if (isIdExists.rows.length === 0) {
+            return res.sendStatus(404);
+        }
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
--- src/routers/clients.routers.js
+++ src/routers/clients.routers.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { getClients, getClientById, postClient, updateClient } from "../controllers/clients.controller.js";
+import { clientValidation, isClientExists } from "../middlewares/clients.middlewares.js";
 
 const router = Router();
 
 router.get("/customers", getClients);
 router.get("/customers/:id", getClientById);
-router.post("/customers", postClient);
-router.put("/customers/:id", updateClient);
+router.post("/customers", clientValidation, postClient);
+router.put("/customers/:id", isClientExists, clientValidation, updateClient);
 
 export default router;
