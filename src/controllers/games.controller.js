6b9cdc9656aa92793fd4a4c8888ef868187a9e44
manuelamp <manuella.m.p182@gmail.com>
2022-12-08 13:14:13 -0300

    feat: getGames and postGame without rules

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -1,5 +1,26 @@
 export async function getGames() {
+
+    const { name } = req.query;
+
+    try {
+        const games = await connection.query("SELECT * FROM games;");
+        res.status(200).send(games.rows);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+
 }
 
 export async function postGame() {
+
+    const {name, image, stockTotal, categoryId, pricePerDay} = req.body;
+
+    try {
+        const game = await connection.query("INSERT INTO games (name, image, stockTotal, categoryId, pricePerDay) VALUES ($1, $2, $3, $4, $5)", 
+        [name, image, stockTotal, categoryId, pricePerDay]);
+        res.sendStatus(201);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+
 }
